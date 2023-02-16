295798076392a488e25f879ddb3b34138f237492
clemlak <clementlakhal@gmail.com>
2023-02-16 16:22:02 +0400

    test: update testDiff_ierfc

--- src/test/Ierfc.t.sol
+++ src/test/Ierfc.t.sol
@@ -12,17 +12,16 @@ contract TestIErfc is Test {
         int256 y = Gaussian.ierfc(x);
     }
 
-    function testDiff_erfc(int256 x) public {
-        vm.assume(x < 1999999999999999998000000000000000002);
-        vm.assume(x > -1999999999999999998000000000000000002);
+    function testDiff_ierfc(int256 x) public {
+        vm.assume(x > 0 && x < 2 ether);
         string[] memory inputs = new string[](3);
         inputs[0] = "./gaussian";
-        inputs[1] = "erfc";
+        inputs[1] = "ierfc";
         inputs[2] = vm.toString(x);
         bytes memory res = vm.ffi(inputs);
         uint256 ref = abi.decode(res, (uint256));
         int256 y = Gaussian.erfc(x);
         // Results have a 0.000000105538072456% difference
-        assertApproxEqAbs(ref, uint256(y), 105538072456);
+        assertApproxEqAbs(ref, uint256(y), 0);
     }
 }
